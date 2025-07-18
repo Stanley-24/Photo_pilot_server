61705fcb059542f201e641bfa763baf41ef7844e
Stanley <owarieta24@outlook.com>
2025-07-18 09:34:20 +0100

    fix: add log to confirm uvicorn runs

--- main.py
+++ main.py
@@ -87,5 +87,6 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))  # Render sets this dynamically
+    port = int(os.environ.get("PORT", 8000))
+    print(f"==== Starting Uvicorn on port {port} ====", flush=True)
     uvicorn.run("main:app", host="0.0.0.0", port=port)
